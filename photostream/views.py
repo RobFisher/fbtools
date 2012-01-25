77564104c5bb7868fe6b9e770e1630ddd8ef2443
Rob Fisher <rfisher@nds.com>
2012-01-25 15:18:42 +0000

    Use template for index page.

--- photostream/views.py
+++ photostream/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from fandjango.decorators import facebook_authorization_required
 
 @facebook_authorization_required
 def index(request):
-    return HttpResponse("Hello, " + request.facebook.user.full_name)
+    template = loader.get_template('index.html')
+    context = RequestContext(request, {
+            'fullname' : request.facebook.user.full_name,
+    })
+    print request.facebook.user.full_name
+    return HttpResponse(template.render(context))
 
